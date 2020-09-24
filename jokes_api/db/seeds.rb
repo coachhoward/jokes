52ce4bf73929f360894fa62379c64142d7355ef5
Chris Howard <choward@ownerfinancellc.com>
2020-09-24 11:16:00 -0400

    Seeded db w/faker Hipster

--- jokes_api/db/seeds.rb
+++ jokes_api/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+100.times do
+    Joke.create(
+      title: Faker::Hipster.sentence(word_count: 8),
+      author: Faker::Team.name,
+      content: Faker::Lorem.paragraph
+    )
+  end
+
+puts "Seeded database"
